048dfec16c8110b181564a84a0dd8919b6b0dc77
Rhys Oxenham <roxenham@redhat.com>
2011-09-13 22:55:06 +0100

    Update build script to give some verbose output

--- build_script.sh
+++ build_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script to build a RHEV-M portable/executable JAR
-
+echo "[rhevm-java] Building latest local code"
 cd src/
 echo "Main-Class: rhevm/MainView" > manifest.txt
 javac rhevm/MainView.java
@@ -8,3 +8,5 @@ jar cvfm rhevm.jar manifest.txt rhevm/*.class
 mv rhevm.jar ../
 rm manifest.txt
 rm rhevm/*.class
+echo "[rhevm-java] SUCCESS: rhevm.jar created"
+echo -e "[rhevm-java] Run \"java -jar rhevm.jar\" to execute" 
